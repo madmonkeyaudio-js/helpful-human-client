b0ad88d46738a3ea199c7c1f7ca09f6663a78294
madmonkeyaudio-js <jonathangross@Jonathans-MBP.hsd1.wa.comcast.net>
2020-01-16 11:24:51 -0800

    initialize project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios'
+
+import Nav from './Components/Navigation/Nav'
+import Content from './Components/Content/Content'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+ class App extends Component {
+
+  state = {
+    colors: []
+  }
+
+
+  componentDidMount() {
+    axios.get('')
+    .then(response => {
+      console.log(response)
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-    </div>
-  );
+      <div className="basic-flex">
+        <Nav/>
+        <Content/>
+      </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
---
+++ src/Components/Navigation/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <div className="nav-container">
+            Navigation 
+        </div>
+    )
+}
+
+export default Nav;
